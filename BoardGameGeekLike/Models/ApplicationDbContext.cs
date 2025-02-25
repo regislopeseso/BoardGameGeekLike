3ebd760fb546dfd78560422acf3775d05da9cba0
Regis <masken.warum_0e@icloud.com>
2025-02-25 18:02:26 -0300

    Configure BoardGame entity

--- BoardGameGeekLike/Models/ApplicationDbContext.cs
+++ BoardGameGeekLike/Models/ApplicationDbContext.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BoardGameGeekLike.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BoardGameGeekLike.Models
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        public DbSet<BoardGame> BoardGames { get; set; }
 
 
     }
--- BoardGameGeekLike/Models/Entities/BoardGame.cs
+++ BoardGameGeekLike/Models/Entities/BoardGame.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace BoardGameGeekLike.Models.Entities
     [Table("boardgames")]
     public class BoardGame
     {
-        Key]
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
