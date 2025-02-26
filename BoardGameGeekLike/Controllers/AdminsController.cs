551cc7d564bf894c99ba81f56e50c8c128b9c177
Regis <masken.warum_0e@icloud.com>
2025-02-26 09:47:20 -0300

    Add AdminsController endpoints

--- BoardGameGeekLike/Controllers/AdminsController.cs
+++ BoardGameGeekLike/Controllers/AdminsController.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BoardGameGeekLike.Models.Dtos.Request;
+using BoardGameGeekLike.Models.Dtos.Response;
+using BoardGameGeekLike.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BoardGameGeekLike.Controllers
@@ -10,6 +13,54 @@ namespace BoardGameGeekLike.Controllers
     [Route("admins/[action]")]
     public class AdminsController : ControllerBase
     {
+        private readonly AdminsService _adminsService;
+
+        public AdminsController(AdminsService adminsService)
+        {
+            this._adminsService = adminsService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBoardGame(AdminsAddBoardGameRequest? request)
+        {
+            var (content, message) = await this._adminsService.AddBoardGame(request);
+
+            var response = new Response<AdminsAddBoardGameResponse>
+            {
+                Content = content,
+                Message = message
+            };
+
+            return new JsonResult(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditBoardGame(AdminsEditBoardGameRequest? request)
+        {
+            var (content, message) = await this._adminsService.EditBoardGame(request);
+
+            var response = new Response<AdminsEditBoardGameResponse>
+            {
+                Content = content,
+                Message = message
+            };
+
+            return new JsonResult(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBoardGame(AdminsDeleteBoardGameRequest? request)
+        {
+            var (content, message) = await this._adminsService.DeleteBoardGame(request);
+
+            var response = new Response<AdminsDeleteBoardGameResponse>
+            {
+                Content = content,
+                Message = message
+            };
+
+            return new JsonResult(response);
+        }
 
     }
 }
